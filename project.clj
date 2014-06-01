73ecb7581dbcc6f0e799bdb92b91f084a9ef3460
Tom Parker <palfrey@tevp.net>
2014-06-01 14:24:31 +0100

    Actually properly set Lein offline mode

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
 	:uberjar-name "moves-beeminder-standalone.jar"
 	:profiles {
 	 :uberjar  {
-	  :offline true
+	  :offline? true
 	  :dependencies [
 	   [com.newrelic.agent.java/newrelic-agent "3.7.0"]
 	  ]
